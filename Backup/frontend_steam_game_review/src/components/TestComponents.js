35d1df3195525efe82ddc834ec8c8e3fa3f938c0
Ju <97508841+haruharuganda@gmail.com>
2023-01-14 14:33:29 +0900

    Backup 지움

--- Backup/frontend_steam_game_review/src/components/TestComponents.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TestComponents = () => {
-  return <div></div>;
-};
-
-export default TestComponents;
--- Backup/frontend_steam_game_review/src/reduex/config/configToolket.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const configToolket = () => {
-  return <div></div>;
-};
-
-export default configToolket;
-
-//test
