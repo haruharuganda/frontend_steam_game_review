5813eaa9bd5c6490494e3d16bbdb29318573f5b5
dong-fa <120082854+dong-fa@users.noreply.github.com>
2023-01-14 19:58:13 +0900

    Detail CSS 작업 완료
    design: detail UI 작업 완료
    
    #1

--- frontend_steam_game_review/src/page/Detail.js
+++ frontend_steam_game_review/src/page/Detail.js
@@ -2,31 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import Head from "../components/Head";
 import Layout from "../components/layout/Layout";
-
+import GameInfo from "../features/gameInfo/GameInfo";
 const Detail = () => {
   return (
     <Layout>
       <Head />
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>v<div>asdasdsad</div>v<div>asdasdsad</div>v
-      <div>asdasdsad</div>v v v v v<div>asdasdsad</div>v v v<div>asdasdsad</div>
-      v v v v v v<div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
-      <div>asdasdsad</div>
+      <GameInfo />
     </Layout>
   );
 };
