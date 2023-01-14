e52ce91c61f02d6da8f5753d0085befffe55c92e
dong-fa <120082854+dong-fa@users.noreply.github.com>
2023-01-14 21:20:47 +0900

    Login CSS 작업완료
    design: login UI 작업 완료
    
    #2

--- frontend_steam_game_review/src/page/Detail.js
+++ frontend_steam_game_review/src/page/Detail.js
@@ -1,14 +1,15 @@
 import React from "react";
-import styled from "styled-components";
+
 import Head from "../components/Head";
 import Layout from "../components/layout/Layout";
 import GameInfo from "../features/gameInfo/GameInfo";
+
 const Detail = () => {
   return (
-    <Layout>
+    <>
       <Head />
       <GameInfo />
-    </Layout>
+    </>
   );
 };
 
