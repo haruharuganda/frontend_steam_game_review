911c3a1cd1c780f5e544b89923e641184f58bbe2
Ju <97508841+haruharuganda@gmail.com>
2023-01-18 00:33:37 +0900

    Mainpage ver.01.

--- frontend_steam_game_review/src/reduex/config/configToolket.js
+++ frontend_steam_game_review/src/reduex/config/configToolket.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSignUp from "../modules/loginSignUp";
 import comments from "../modules/commentsSlice";
-
+import mainSlice from "../modules/mainSlice";
 const store = configureStore({
-  reducer: { loginSignUp: loginSignUp, comments },
+  reducer: { loginSignUp: loginSignUp, comments, mainSlice },
 });
 
 export default store;
